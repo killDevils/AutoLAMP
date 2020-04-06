20621ea04d5d74794562d57add9994d8b92abdfc
killDevils <55968229+killDevils@users.noreply.github.com>
2020-04-06 14:02:38 +0800

    Update initialisation.sh

--- initialisation.sh
+++ initialisation.sh
@@ -1,2 +1,12 @@
+#launch instances
 curl -s -O https://raw.githubusercontent.com/killDevils/AutoLAMP/master/ubuntu_18.04.sh
 bash ubuntu_18.04.sh
+
+
+
+
+#auto deploy LAMP
+curl -s -O https://raw.githubusercontent.com/killDevils/AutoLAMP/master/ubuntu_18.04.sh
+bash ubuntu_18.04.sh
+
+
